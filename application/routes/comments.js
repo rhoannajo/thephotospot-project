f6f2f49106e3190ae9ee60cb86c3d273fc91f10d
RJ Perez <rhoannajo@gmail.com>
2021-05-11 23:29:15 -0700

    implemented create comment route

--- application/routes/comments.js
+++ application/routes/comments.js
@@ -5,9 +5,42 @@ const errorPrint = require("../helpers/debug/debugprinters").errorPrint;
 const { create } = require("../models/comments");
 
 
-router.post('/create', (req, res, nest) => {
-    console.log(req.body);
-    res.json("We got your comment");
+router.post('/create', (req, res, next) => {
+    if (!req.session.username) {
+        errorPrint("must be logged in to comment");
+        res.json({
+            code: -1,
+            status: "alert",
+            message: "Must be logged in to create a comment",
+        })
+    } else {
+        let {comment, postId} = req.body;
+        let username = req.session.username;
+        let userId = req.session.userId;
+        create(userId, postId, comment)
+        .then((wasSuccessful) => {
+            if (wasSuccessful != -1) {
+                successPrint(`comment was created for ${username}`);
+                res.json({
+                    code: 1,
+                    status: "success",
+                    message: "comment created",
+                    comment: comment,
+                    username: username
+                })
+            } else {
+                errorPrint('comment was not saved');
+                res.json({
+                    code: -1,
+                    status: "alert",
+                    message: "Comment was not created",
+    
+                })
+            }
+        })
+        .catch((err) => next(err));
+    }
+
 })
 
 
